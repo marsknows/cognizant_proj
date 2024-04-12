3415c26f25463674ce6ccb54666ac70a2bbacfc6
marsknows <88752025+marsknows@users.noreply.github.com>
2024-04-12 16:45:54 -0700

    Initial commit

---
+++ tests/cognizant.spec.js
@@ -0,0 +1,33 @@
+const { test, expect } = require('@playwright/test');
+test('cognizant test', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await expect(page.locator('[data-test="item-4-img-link"]')).toBeVisible();//items need to come from a csv input file
+  await expect(page.locator('[data-test="item-0-img-link"]')).toBeVisible();//items need to come from a csv input file
+  await expect(page.locator('[data-test="item-1-img-link"]')).toBeVisible();//items need to come from a csv input file
+  await expect(page.locator('[data-test="item-5-img-link"]')).toBeVisible();//items need to come from a csv input file
+  await expect(page.locator('[data-test="item-3-img-link"]')).toBeVisible();//items need to come from a csv input file
+  await expect(page.locator('[data-test="item-2-img-link"]')).toBeVisible();//items need to come from a csv input file
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').fill('Mars');
+  await page.locator('[data-test="lastName"]').fill('Knows');
+  await page.locator('[data-test="postalCode"]').fill('95051');
+  await page.locator('[data-test="continue"]').click();
+  await page.pause();
+  await expect(page.locator('[data-test="total-label"]')).toHaveText("Total: $49.66");
+//   await expect(page.locator('[data-test="total-label"]')).toHaveValue();//need to check if total is >< $40
+  await page.locator('[data-test="finish"]').click();//if <$40 click finish
+//   await page.locator('[data-test="cancel"]').click();//if >$40 click cancel
+  await page.locator('[data-test="complete-header"]').isVisible();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
