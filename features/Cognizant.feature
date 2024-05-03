1c83ff9286efd5a09cede96543f30d14e6129c77
marsknows <88752025+marsknows@users.noreply.github.com>
2024-05-03 12:54:32 -0700

    implement steps git status clear

--- features/Cognizant.feature
+++ features/Cognizant.feature
@@ -4,7 +4,6 @@ Feature: Cognizant Assignment
 
 Scenario: Saucedemo practice
 Given The user logs in
-And the user navigates to the home page
 And the user clicks the add backpack element
 And the user clicks the add bike light element
 And the user clicks the add t shirt element
@@ -15,25 +14,18 @@ And the the user validates the jacket element is visible
 And the the user validates the onesie element is visible
 And the the user validates the red shirt element is visible
 And the user clicks the cart element
-And the user navigates to the cart page
 And the user clicks the remove bike element
 And the user clicks the checkout element
-And the user navigates to the checkout page
 And the user fills the page with user info
 And the user clicks the continue element
 And the user clicks the t shirt element
-And the user navigates to the bolt shirt page
 And the user clicks the remove element
 And the user clicks the cart element
-And the user navigates to the cart page
 And the user clicks the checkout element
-And the user navigates to the checkout page
 And the user fills the page with user info
 And the user clicks the continue element
 When the user clicks the finish element
-And the user navigates to the confirmation page
 Then the the user validates the confirmation element is visible
 And the user clicks the back home element
-And the user navigates to the home page
 And the user clicks the menu element
 And the user clicks the logout element
--- features/step_definitions/Cognizant_steps.js
+++ features/step_definitions/Cognizant_steps.js
@@ -1,4 +1,5 @@
 const {Given,When,Then}= require ('@cucumber/cucumber')
+const { expect } = require('@playwright/test');
 
 Given('The user logs in', async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
@@ -7,22 +8,88 @@ Given('The user logs in', async ({page}) => {
     await page.locator('[data-test="login-button"]').click();
   });
 
-  Given('the user clicks the {String} element', function (String) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  Given('the user clicks the add backpack element', async ({page}) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   });
 
-  Given('the user fills the page with {String}', function (String) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  Given('the user clicks the add bike light element', async ({page}) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
   });
 
-  Given('the the user validates the {String} element is visible', function (String) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  Given('the user clicks the add t shirt element', async ({page}) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
   });
 
-  Then('the user navigates to the {String} page', function (String) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  Given('the the user validates the backpack element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="item-4-img-link"]')).toBeVisible();
+  });
+
+  Given('the the user validates the bike light element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="item-0-img-link"]')).toBeVisible();
+  });
+
+  Given('the the user validates the t shirt element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="item-1-img-link"]')).toBeVisible();
+  });
+
+  Given('the the user validates the jacket element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="item-5-img-link"]')).toBeVisible();
+  });
+
+  Given('the the user validates the onesie element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="item-3-img-link"]')).toBeVisible();
+  });
+
+  Given('the the user validates the red shirt element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="item-2-img-link"]')).toBeVisible();
+  });
+
+  Given('the user clicks the cart element', async ({page}) => {
+    await page.locator('[data-test="shopping-cart-link"]').click();
+  });
+
+  Given('the user clicks the remove bike element', async ({page}) => {
+    await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  });
+
+  Given('the user clicks the checkout element', async ({page}) => {
+    await page.locator('[data-test="checkout"]').click();
+  });
+
+  Given('the user fills the page with user info', async ({page}) => {
+    await page.locator('[data-test="firstName"]').fill('Mario');
+    await page.locator('[data-test="lastName"]').fill('J');
+    await page.locator('[data-test="postalCode"]').fill('95051');
+  });
+
+  Given('the user clicks the continue element', async ({page}) => {
+    await page.locator('[data-test="continue"]').click();
+  });
+
+  Given('the user clicks the t shirt element', async ({page}) => {
+    await page.locator('[data-test="item-1-title-link"]').click();
+  });
+
+  Given('the user clicks the remove element', async ({page}) => {
+    await page.locator('[data-test="remove"]').click();
+  });
+
+  When('the user clicks the finish element', async ({page}) => {
+    await page.locator('[data-test="finish"]').click();
+  });
+
+  Then('the the user validates the confirmation element is visible', async ({page}) => {
+    await expect(page.locator('[data-test="complete-header"]')).isVisible();
+  });
+
+  Then('the user clicks the back home element', async ({page}) => {
+    await page.locator('[data-test="back-to-products"]').click();
+  });
+
+  Then('the user clicks the menu element', async ({page}) => {
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+  });
+
+  Then('the user clicks the logout element', async ({page}) => {
+    await page.locator('[data-test="logout-sidebar-link"]').click();
   });
